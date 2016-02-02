5468bf8c91038596e37899639489acd2d8a4c716
faylite <benvev95@gmail.com>
2016-02-02 23:05:34 +0100

    Added a utility class for utility functions. Added get date method, will be used for the database timestamp

---
+++ SaltyBet-Desktop/Util.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaltyBet_Desktop
+{
+	class Util
+	{
+		/// <summary>
+		/// Returns a string of the current date and time in the format: 
+		/// YYYY/MM/DD-HH:MM:SS
+		/// </summary>
+		/// <returns></returns>
+		public string getLongDate()
+		{
+			return
+				DateTime.Now.Year	+ "/" +
+				DateTime.Now.Month	+ "/" +
+				DateTime.Now.Day	+ "-" +
+				DateTime.Now.Hour	+ ":" +
+				DateTime.Now.Minute
+			;
+		}
+	}
+}
