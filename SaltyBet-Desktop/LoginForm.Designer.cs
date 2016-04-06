cd636fa7e0119b731f0532d6c33e5a74ddf786d8
Faylite <benvev95@gmail.com>
2016-04-06 17:35:42 +0200

    Minor changes to login form

--- SaltyBet-Desktop/LoginForm.Designer.cs
+++ SaltyBet-Desktop/LoginForm.Designer.cs
@@ -46,6 +46,8 @@
             // 
             // tbUsername
             // 
+            this.tbUsername.AllowDrop = true;
+            this.tbUsername.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.tbUsername.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tbUsername.Location = new System.Drawing.Point(13, 13);
             this.tbUsername.Name = "tbUsername";
@@ -54,10 +56,12 @@
             // 
             // tbPassword
             // 
+            this.tbPassword.AllowDrop = true;
+            this.tbPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.tbPassword.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tbPassword.Location = new System.Drawing.Point(12, 48);
             this.tbPassword.Name = "tbPassword";
-            this.tbPassword.Size = new System.Drawing.Size(462, 29);
+            this.tbPassword.Size = new System.Drawing.Size(463, 29);
             this.tbPassword.TabIndex = 4;
             // 
             // LoginForm
@@ -68,8 +72,11 @@
             this.Controls.Add(this.tbPassword);
             this.Controls.Add(this.tbUsername);
             this.Controls.Add(this.btnLogin);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "LoginForm";
-            this.Text = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.TopMost = true;
             this.ResumeLayout(false);
             this.PerformLayout();
 
