14ff16c364ce3b573e83ddfa76b80437a1d0ea39
Faylite <benvev95@gmail.com>
2016-04-06 20:54:28 +0200

    Fixed appsettings write method

--- SaltyBet-Desktop/Account.cs
+++ SaltyBet-Desktop/Account.cs
@@ -114,8 +114,30 @@ namespace SaltyBet_Desktop
             byte[] plainPassword = Encoding.UTF8.GetBytes(password);
             byte[] encryptedPassword = ProtectedData.Protect(plainPassword, entropy, DataProtectionScope.CurrentUser);
 
-            ConfigurationManager.AppSettings.Add("email", encryptedEmail.ToString());
-            ConfigurationManager.AppSettings.Add("password", encryptedPassword.ToString());
+            
+            AddUpdateAppSetting("email", encryptedEmail.ToString());
+            AddUpdateAppSetting("password", encryptedPassword.ToString());
+        }
+
+        public void AddUpdateAppSetting(string key, string value)
+        {
+            try
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var settings = configFile.AppSettings.Settings;
+
+                if (settings[key] == null)
+                    settings.Add(key, value);
+                else
+                    settings[key].Value = value;
+
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }
+            catch (ConfigurationErrorsException)
+            {
+                MessageBox.Show("Couldn't save your email and password", "Error");
+            }
         }
     }
 }
